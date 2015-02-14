4888251546f929939a4795403e10c9adf35e6af1
jeroenstevens <jeroenstevens@live.nl>
2015-02-14 18:06:48 +0100

    - question controller tests + question integration tests + question value attribute + jbuilder view

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214154029) do
+ActiveRecord::Schema.define(version: 20150214164520) do
 
   create_table "questions", force: :cascade do |t|
+    t.string "value"
   end
 
 end
---
+++ test/integration/questions_integration_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class QuestionsIntegrationTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @question = Question.create!
+  end
+
+  test 'route : questions' do
+    get '/questions.json'
+    assert_response :success
+  end
+
+  test 'route : questions/{id}' do
+    get "/questions/#{@question.id}.json"
+    assert_response :success
+  end
+
+  test 'question : id' do
+    get "/questions/#{@question.id}.json"
+    assert_equal json['id'], @question.id
+  end
+
+  test 'question : value' do
+    @question.value = 'Test'
+    @question.save
+
+    get "/questions/#{@question.id}.json"
+
+    assert_equal @question.value, json['value']
+  end
+
+  def json
+    @json ||= JSON.parse(response.body)
+  end
+
+end
